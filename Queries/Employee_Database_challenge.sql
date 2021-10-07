6436b44b28a290b0f68b12095279decaf033663f
tessiertodd <tessiert@yahoo.com>
2021-10-07 01:03:31 -0400

    Module 7 Challenge Assignement Deliverable 1 and 2

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,32 @@
+-- Provide employee info and title for employees born between 1952 and 1955
+SELECT e.emp_no, e.first_name, e.last_name, t.title, t.from_date, t.to_date
+INTO retirement_titles
+FROM employees AS e
+JOIN titles AS t
+ON (e.emp_no=t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no, first_name, last_name, title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, to_date DESC;
+
+-- Retrieve number of employees by their most recent job title who are about to retire
+SELECT COUNT(emp_no) AS "People retiring", title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY "People retiring" DESC;
+
+-- Create a mentorship-eligible table for employees born in 1965
+SELECT DISTINCT ON(e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date, de.to_date, t.title
+INTO mentorship_eligibility
+FROM employees AS e
+JOIN dept_emp AS de
+ON (e.emp_no=de.emp_no)
+JOIN titles AS t
+ON(t.emp_no=e.emp_no)
+WHERE de.to_date='9999-01-01'AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
--- schema.sql
+++ schema.sql
@@ -30,7 +30,7 @@ CREATE TABLE salaries(
 	emp_no INT NOT NULL,
 	salary INT NOT NULL,
 	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
+	to_data DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	PRIMARY KEY (emp_no)
 );
@@ -47,9 +47,8 @@ CREATE TABLE dept_manager(
 
 CREATE TABLE titles(
 	emp_no INT NOT NULL,
-	title VARCHAR NOT NULL,
-	from_data DATE NOT NULL,
+	title VARCHAR (50) NOT NULL,
+	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
